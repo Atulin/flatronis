0d1282e93c229691221be305963643360d33fec4
Angius <koumarin@gmail.com>
2018-11-07 08:32:55 +0100

    Parallax thingy

---
+++ public/controllers/Home.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Angius
+ * Date: 07.11.2018
+ * Time: 06:11
+ */
+
+// Load up Twig stuff
+$loader = new Twig_Loader_Filesystem(array(
+        dirname(__DIR__,1).'/views',
+        dirname(__DIR__,1).'/assets')
+);
+$twig = new Twig_Environment($loader);
+$twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) {
+    return sprintf('/public/%s', ltrim($asset, '/'));
+}));
+
+// Render Twig template
+try {
+    // Render the actual Twig template
+    echo $twig->render('home.twig', array(
+
+    ));
+
+    // Handle all possible errors
+} catch (Twig_Error_Loader $e) {
+    header('Content-type: application/json');
+    echo json_encode('Error [1]: '.$e);
+} catch (Twig_Error_Runtime $e) {
+    header('Content-type: application/json');
+    echo json_encode('Error [2]: '.$e);
+} catch (Twig_Error_Syntax $e) {
+    header('Content-type: application/json');
+    echo json_encode('Error [3]: '.$e);
+}
