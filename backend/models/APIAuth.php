706cc9b77271f80e3600bfa9592561179fb6095c
Angius <koumarin@gmail.com>
2019-02-18 22:33:45 +0100

    API auth encryption  – Added Encrypt() method to encrypt the API key  – Added Check() method to check for API key validity

--- backend/models/APIAuth.php
+++ backend/models/APIAuth.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Angius
@@ -16,8 +17,11 @@ class APIAuth
 {
     public $name;
     public $comment;
+
     private $public_key;
     private $private_key;
+    private $cipher = 'aes-128-gcm';
+
 
     /**
      * @return string
@@ -51,4 +55,43 @@ class APIAuth
     }
 
 
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    public function Encrypt(): string
+    {
+        $data       = $this->public_key;
+        $key        = $this->private_key;
+        $cipher     = $this->cipher;
+        $iv_len     = openssl_cipher_iv_length($cipher);
+        $tag_length = 16;
+        $iv         = random_bytes($iv_len);
+        $tag        = ''; // will be filled by openssl_encrypt
+
+        $ciphertext = openssl_encrypt($data, $cipher, $key, OPENSSL_RAW_DATA, $iv, $tag, '', $tag_length);
+
+        return base64_encode($iv.$tag.$ciphertext);
+    }
+
+    /**
+     * @param string $received
+     * @return bool
+     */
+    public function Check(string $received): bool
+    {
+        $public     = $this->public_key;
+        $key        = $this->private_key;
+        $cipher     = $this->cipher;
+        $encrypted  = base64_decode($received);
+        $iv_len     = openssl_cipher_iv_length($cipher);
+        $tag_length = 16;
+        $iv         = substr($encrypted, 0, $iv_len);
+        $tag        = substr($encrypted, $iv_len, $tag_length);
+        $ciphertext = substr($encrypted, $iv_len + $tag_length);
+
+        $decrypted = openssl_decrypt($ciphertext, $cipher, $key, OPENSSL_RAW_DATA, $iv, $tag);
+
+        return $public === $decrypted;
+    }
 }
